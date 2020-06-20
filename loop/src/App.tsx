d933271656db90a50b9661f3fa6bfcf85e1afb8c
Brad Melchin <melchin.brad@gmail.com>
2020-06-20 14:55:03 -0600

    Changed my mind about removing these files for now

--- loop/src/App.tsx
+++ loop/src/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { IntlProvider } from "react-intl";
 
 import { Paginator } from "generics";
 
 function App() {
   const [index, setIndex] = React.useState<number>(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <Paginator
-          setPageIndex={setIndex}
-          perPage={10}
-          pageIndex={index}
-          totalRecords={200}
-        />
-      </header>
-    </div>
+    <IntlProvider locale={"en"}>
+      <div className="App">
+        <header className="App-header">
+          <Paginator
+            setPageIndex={setIndex}
+            perPage={10}
+            pageIndex={index}
+            totalRecords={200}
+          />
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
